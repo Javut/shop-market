a0b49c795b7549e2b13296f74799da99e806d536
javut <jaider.vergara@globalbank.com.pa>
2022-03-24 18:14:04 -0500

    first commit

---
+++ src/main/java/com/shopmarket/ShopMarketApplication.java
@@ -0,0 +1,13 @@
+package com.shopmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopMarketApplication.class, args);
+	}
+
+}
